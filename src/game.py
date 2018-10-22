9aba65875d4e1bdcf7571cb9ea42c4222102a6f2
soziele <sonia.ziele@gmail.com>
2018-10-22 18:19:01 +0200

    message

--- src/game.py
+++ src/game.py
@@ -1,10 +1,16 @@
 from states.gameState import GameState
+from player.player import Player
+
 
 class Game:
+
     def __init__(self):
         self.is_running = True
         self.GameState = GameState()
+        self.Player=Player(input, )
 
     def run(self):
         while(self.GameState.state != GameState.State.EXIT):
             self.GameState.Update()
+
+        
--- src/player/player.py
+++ src/player/player.py
@@ -1,4 +1,8 @@
 class Player:
+    
     def __init__(self, name, room):
         self.name = name
-        self.currentLocation = room
+        self.current_location = room
+    name=input("Prosze podac imie gracza: ")
+
+        
