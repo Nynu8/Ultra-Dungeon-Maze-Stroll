9a0380414ca3714c6f62417d5b55e752b547caa8
soziele <sonia.ziele@gmail.com>
2018-10-24 18:11:58 +0200

    labyrinth update

--- src/map/room.py
+++ src/map/room.py
@@ -6,11 +6,21 @@ class Room:
         EAST = 2
         SOUTH = 3
         WEST = 4
-
+    class Type(Enum):
+        COMMON = 1
+        PORTAL = 2
+        WITH_KEY = 3
+        EXIT = 4
+        
     def __init__(self):
         self.id = uuid.uuid4()
         self.route = {}
+        self.type = ()
 
     def add_route(self, room_id, direction):
         self.route[room_id] = direction
+
+    def add_type(self, room_type=Room.Type.COMMON):
+        self.type = room_type
+    
     
